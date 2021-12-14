3bdf9d0517b29dab2828af46bb4dadb68e49efaa
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2021-12-14 21:08:55 +0900

    indexOf, includes

---
+++ Hashmap-Hashtable_ransomnote.js
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+// magazine의 단어를 통째로 가져다 원하는 문장을 온전히 만들 수 있으면 Print 'Yes', if not 'No'
+// * No value return 
+
+/*
+ * Complete the 'checkMagazine' function below.
+ *
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY magazine
+ *  2. STRING_ARRAY note
+ */
+
+function checkMagazine(magazine, note) {
+  
+}
+
+console.log(checkMagazine('give me one grand today night', 'give one grand today')); // Yes
+console.log(checkMagazine('two times three is not four', 'two times two is four')); // No
+console.log(checkMagazine('ive got a lovely bunch of coconuts', 'ive got some coconuts')); // No
